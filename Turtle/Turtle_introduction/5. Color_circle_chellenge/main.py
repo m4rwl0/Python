593618c035d3657f84f243495c11d4d4b195a2db
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-20 15:06:56 +0100

    Challenge of colours in turte
    First chellange

---
+++ Turtle/Turtle_introduction/5. Color_circle_chellenge/main.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+# As a challenge, create three circles – one green, one red, and one purple.
+
+
+color("green")
+circle(50)
+
+color("red")
+
+circle(55)
+
+color("purple")
+
+circle(60)
+
+done()
