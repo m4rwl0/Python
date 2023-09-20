e2358fe6553bb1e8ec1a7c5fcd3c641dc5bfe956
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-20 15:12:10 +0100

    Turtle lifting the pen
    penup()
    pendown()

---
+++ Turtle/Turtle_introduction/10. Lifting_pen/main.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+circle(50)
+forward(120)
+circle(50)
+# with forward we can see the line between cricles
+
+
+# penup()
+# puting pen to the air
+
+# pendown()
+# puting pen to the table
+
+penup()
+forward(120)
+pendown()
+circle(50)
+
+done()
