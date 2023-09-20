b401136a926a4c9fe228deac3258d8fc05bb8270
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-20 15:07:43 +0100

    Filling a shapes
    commends:
    begin_fill()
    end_fill()

---
+++ Turtle/Turtle_introduction/6. Filling_shapes/main.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+
+# fill the circle shape
+color("red")
+begin_fill()
+circle(50)
+end_fill()
+
+
+forward(200)
+
+# fill square shape
+color("blue")
+begin_fill()
+forward(100)
+right(90)
+
+forward(100)
+right(90)
+
+forward(100)
+right(90)
+
+forward(100)
+right(90)
+end_fill()
+
+done()
