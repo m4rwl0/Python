704eebad272c79f3dba85f80dd55fce0309b4571
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-20 15:08:47 +0100

    Chellenge of filling the colours
    begin_fill()
    end_fill()

---
+++ Turtle/Turtle_introduction/7. Challenge_filling_shape/main.py
@@ -0,0 +1,20 @@
+# As a bit of a challenge, draw and fill in an equilateral triangle.
+# An equilateral triangle, is a triangle where all sides are of equal length, thus all of the angles must be the same too. For this challenge, make each side of the triangle 100 pixels.
+
+from turtle import *
+
+color("green")
+begin_fill()
+forward(100)
+left(120)
+
+forward(100)
+left(120)
+
+forward(100)
+left(120)
+
+
+end_fill()
+
+done()
