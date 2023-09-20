d454a1cca403ff91ce1d2eb4d724f06f61addc02
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-20 15:11:23 +0100

    Chellenge background color
    getscreen()
    Return the TurtleScreen object, the turtle is drawing on.

---
+++ Turtle/Turtle_introduction/9. Challange_bg_color/main.py
@@ -0,0 +1,9 @@
+from turtle import *
+
+getscreen().bgcolor("#ffd000")
+circle(50)
+
+# if you are using a local environment such as
+# Visual Studio Code, add "done()" at the end
+# to prevent the output window from closing
+done()
