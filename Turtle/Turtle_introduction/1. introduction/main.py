dc540969335eff909fec3a0ad6c55e07960c8fe0
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-20 15:04:25 +0100

    Turtle introduction
    forward/right moving around

---
+++ Turtle/Turtle_introduction/1. introduction/main.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+# from turtle import everything
+
+forward(20)
+# move turtle forward (amount of the pixels)
+
+right(90)
+# right (how many degrees)
+
+forward(100)
+
+done()
+# prevent closing the window
