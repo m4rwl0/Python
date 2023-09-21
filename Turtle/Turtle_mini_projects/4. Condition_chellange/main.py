ea411c7ddfb83e6b59f77a3c5e6edfccb6012051
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-21 11:26:27 +0100

    Condition challenge
    Condition chellange

---
+++ Turtle/Turtle_mini_projects/4. Condition_chellange/main.py
@@ -0,0 +1,23 @@
+# With conditions, we can now decide which parts of our code to run based on a test. Below, you can experiment with conditions. Try also utilizing else if and else statements.
+
+
+a = 5
+b = 5
+
+if a == b:
+    print("a is equal to b")
+
+if a > b:
+    print("a is greater than b")
+
+if a < b:
+    print("a is less than b")
+
+if a != b:
+    print("a doesn't equal b")
+
+if a >= b:
+    print("a is greater than or equals to b")
+
+if a <= b:
+    print("a is less than or equals to b")
---
+++ Turtle/Turtle_mini_projects/4. Condition_chellange/result.py
@@ -0,0 +1,23 @@
+a = 5
+b = 5
+
+if a == b:
+    print("a is equal to b")
+
+elif a > b:
+    print("a is greater than b")
+
+elif a < b:
+    print("a is less than b")
+
+elif a != b:
+    print("a doesn't equal b")
+
+elif a >= b:
+    print("a is greater than or equals to b")
+
+elif a <= b:
+    print("a is less than or equals to b")
+
+else:
+    print("I think that you made mistake!")
