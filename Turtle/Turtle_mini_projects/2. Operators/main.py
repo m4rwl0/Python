9bd6f0a031a513a7683c82c91b29fc0cd9b448dc
Martyna Wloka <138770392+marwlok@users.noreply.github.com>
2023-09-21 11:25:04 +0100

    Variable and operators
    Small recap

---
+++ Turtle/Turtle_mini_projects/2. Operators/main.py
@@ -0,0 +1,36 @@
+# player hp
+health = 100
+
+# player is taking damage 20
+health = health - 20
+
+print(health)
+
+# player is leveling up the hp
+health = health + 50
+
+print(health)
+
+# player has a boost
+
+health = health * 1.5
+
+print(health)
+
+# player has downgrade
+
+health = health / 2
+
+
+# also
+
+
+num = 10
+# increase num by 5
+num += 5
+# subtract 3 from num
+num -= 3
+# multiply num by 1.5
+num *= 1.5
+# divide num by 2
+num /= 2
